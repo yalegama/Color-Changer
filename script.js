f23dfcb4f28824612a06de27ef2c09cd3f1c4e3a
Yalegama <nadunyalegama@gmai.com>
2021-08-31 09:27:43 +0530

    second commit

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-let colors = ['red', 'green', 'blue', 'white', 'black', 'orange'];
-let btn = document.getElementById('button');
-let text = document.getElementById('text');
-let colorLoader = 0;
-btn.addEventListener('click', () => {
-    colorLoader++;
+let btn=document.getElementById('button');
+let text=document.getElementById('text');
 
-    function loadion() {
-        if (colorLoader < colors.length - 1) {
-            document.body.style.backgroundColor = colors[colorLoader];
-        } else {
-            document.body.style.backgroundColor = colors[colorLoader];
-        }
-    } loadion();
-});
+let colcors=['red','green','blue','orange','black','white'];
+
+btn.addEventListener('click',colorChange);
+
+function colorChange(){
+    let random=Math.floor(Math.random()*colcors.length);
+    document.body.style.backgroundColor=colcors[random];
+    text.innerHTML=colcors[random];
+}
+
+/**hex Colour Changing */
+
+let hexBtn=document.getElementById('hex');
+let hexColor=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+
+hexBtn.addEventListener('click',changeHexColor);
+
+function changeHexColor(){
+    let first='#';
+
+    for(let i=0;i<6;i++){
+        first+=hexColor[getHexColor()];
+        console.log(first);
+    }
+    document.body.style.backgroundColor=first;
+    text.innerHTML=first;
+
+}
+function getHexColor(){
+    return Math.floor(Math.random()*hexColor.length);
+}
